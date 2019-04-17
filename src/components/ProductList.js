a63dba55c7b555795c68316d1293ac5cf2760fe9
Luis Cuzquen <cuzquenjr@gmail.com>
2019-04-16 17:18:06 -0700

    Product list state

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-// import Product from "./Product";
+import Product from "./Product";
 import Title from './Title';
 import {storeProducts} from '../data';
+import {ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
   state = {
@@ -15,7 +16,16 @@ export default class ProductList extends Component {
           <div className="py-5">
             <div className="container">
             <Title name='our' title='products'/>
-              <div className="row" />
+
+              <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map( product =>{
+                    return <Product key ={product.id} product={product}/>
+                  })
+                }}
+                </ProductConsumer>
+              </div>
             </div>
           </div>
         </React.Fragment>
