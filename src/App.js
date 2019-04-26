4885f4d0d48079af6e7e946de11a22d679c05be3
Luis Cuzquen <cuzquenjr@gmail.com>
2019-04-26 14:06:58 -0700

    modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/NavBar';
-import ProductList from './components/ProductList';
-import Details from './components/Details';
-import Cart from './components/Cart';
-import Default from './components/Default';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/NavBar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Modal from "./components/Modal";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar/>
+        <Navbar />
         <Switch>
-          <Route exact path='/' component={ProductList}/>
-          <Route path='/details' component={Details}/>
-          <Route path='/cart' component={Cart}/>
-          <Route component={Default}/>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
         </Switch>
+        <Modal />
       </React.Fragment>
     );
   }
